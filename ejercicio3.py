903c97a2cf42c375ae2043a0309a390fd29faa57
Pedro <pedromorfeu@gmail.com>
2016-09-11 22:09:47 +0200

    fix ejercicio 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -27,7 +27,6 @@ filter_RDD = split_RDD.map(lambda x : FinancialData(x[0],x[1],x[2],x[3],x[4],x[5
 print(filter_RDD.take(5))
 
 quarter_close_RDD = filter_RDD.map(lambda x : (str(x.Date.year) +"-Q"+ str((x.Date.month-1)//3), (x.Close, 1)))\
-    .reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
-    # \
-    # map(lambda x: (x[0], x[1][0]/x[1][1]))
+    .reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))\
+    .map(lambda x: (x[0], x[1][0]/x[1][1]))
 print(quarter_close_RDD.take(10))
