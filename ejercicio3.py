189a0ed9573cdd5e4b1ce4ac07f098401a3e35fb
Pedro <pedromorfeu@gmail.com>
2016-09-12 22:09:30 +0200

    ejercicio 3 with def mean

--- ejercicio3.py
+++ ejercicio3.py
@@ -8,6 +8,12 @@ from pyspark import SparkContext
 
 from operator import add
 
+
+def mean(_list):
+    result = sum(_list)/len(_list)
+    return round(result, 6)
+
+
 print("*** Ejercicio 3 ***")
 
 # create Spark context
@@ -26,7 +32,8 @@ filter_RDD = split_RDD.map(lambda x : FinancialData(x[0],x[1],x[2],x[3],x[4],x[5
     .filter(lambda x : x.Date.year in [2015,2016])
 print(filter_RDD.take(5))
 
-quarter_close_RDD = filter_RDD.map(lambda x : (str(x.Date.year) +"-Q"+ str((x.Date.month-1)//3), (x.Close, 1)))\
-    .reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))\
-    .map(lambda x: (x[0], x[1][0]/x[1][1]))
-print(quarter_close_RDD.take(10))
+quarter_close_RDD = filter_RDD.map(lambda x : (str(x.Date.year) +"-Q"+ str((x.Date.month-1)//3), x.Close))\
+    .groupByKey()\
+    .map( lambda x: (x[0], mean(list(x[1]))) )
+print(quarter_close_RDD.collect())
+
